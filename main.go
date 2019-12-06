8e3f8c05b50cd3bb41609daac1aff75a1fe0647c
youshy <devilyoushy@gmail.com>
2019-12-06 14:31:14 +0000

    Package done

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os/exec"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"golang.org/x/sync/semaphore"
+)
+
+type PortScanner struct {
+	ip   string
+	lock *semaphore.Weighted
+}
+
+func Ulimit() int64 {
+	out, err := exec.Command("ulimit", "-n").Output()
+	if err != nil {
+		log.Printf("Unable to run error: %s\n", err)
+	}
+
+	s := strings.TrimSpace(string(out))
+
+	i, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		log.Printf("Unable to parse int: %s\n", err)
+	}
+
+	return i
+}
+
+func ScanPort(ip string, port int, timeout time.Duration) {
+	target := fmt.Sprintf("%s:%d", ip, port)
+	conn, err := net.DialTimeout("tcp", target, timeout)
+
+	if err != nil {
+		if strings.Contains(err.Error(), "too many open files") {
+			time.Sleep(timeout)
+			ScanPort(ip, port, timeout)
+		}
+		return
+	}
+
+	conn.Close()
+	log.Println("port", port, "open")
+}
+
+func (ps *PortScanner) Start(f, l int, timeout time.Duration) {
+	wg := sync.WaitGroup{}
+	defer wg.Wait()
+
+	for port := f; port <= l; port++ {
+		ps.lock.Acquire(context.TODO(), 1)
+		wg.Add(1)
+		go func(port int) {
+			defer ps.lock.Release(1)
+			defer wg.Done()
+			ScanPort(ps.ip, port, timeout)
+		}(port)
+	}
+}
+
+func main() {
+	ps := &PortScanner{
+		ip:   "127.0.0.1",
+		lock: semaphore.NewWeighted(Ulimit()),
+	}
+	ps.Start(2500, 25000, 500*time.Millisecond)
+}
